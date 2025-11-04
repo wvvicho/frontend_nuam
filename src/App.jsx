89425c8eadee3a20121a184eab8cc14c3f5b0d53
wvvicho <vicente.rosales05gmail.com>
2025-11-03 22:30:16 -0300

    Agregando estado de filtros de búsqueda al componente App para poder traspasarlos del componente Búsqueda al componente Tabla

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import Busqueda from "./Búsqueda calificaciones/Busqueda";
+import Tabla from "./Tabla calificaciones/Tabla";
+import { useState } from "react";
 
 function App() {
+
+  const [filtros, setFiltros] = useState({
+          mercado: '',
+          origen: '',
+          periodo: '',
+          pendiente: false
+      });
+
+  const Cambiando = valor => (
+      setFiltros(valor)
+  );
+
   return (
     <div className="App">
       <h1 className="text-primary">Calificaciones Tributarias</h1>
       <hr/>
-      <Busqueda />
+      <Busqueda Cambiando={Cambiando}/>
+      <Tabla mercadoBusqueda={filtros.mercado} origenBusqueda={filtros.origen} periodoBusqueda={filtros.periodo} pendienteBusqueda={filtros.pendiente} />
     </div>
   );
 }
