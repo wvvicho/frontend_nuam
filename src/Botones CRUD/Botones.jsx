b198d17a25391a3be09acb238375e40c6f001f1e
wvvicho <vicente.rosales05@gmail.com>
2025-11-07 23:35:40 -0300

    Agregados estilos para Modal de Ingreso

--- src/Botones CRUD/Botones.jsx
+++ src/Botones CRUD/Botones.jsx
@@ -1,4 +1,4 @@
-import  Modal  from "@mui/material/Modal";
+import {  Modal, Box } from "@mui/material";
 import { useState } from "react";
 
 
@@ -9,23 +9,35 @@ function Botones ({ manejarBotonIngreso }) {
     const manejarAbrir = () => setAbrir(true);
     const manejarCerrar = () => setAbrir(false);
 
-
+    const estiloModal = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        bgcolor: 'white',
+        p: 4,
+        width: '70%',
+        height:'70%',
+        border:'2px solid lightblue',
+        borderRadius:'15px',
+        textAlign: 'center',
+        boxShadow: '5px 5px 10px 0px black'
+    }
 
     return (
         <div className="d-flex gap-1">
             <button className="btn btn-primary on" onClick={manejarAbrir}>INGRESAR</button>
             <Modal
-            className="modal-dialog modal-xl"
             open={abrir}
             onClose={manejarCerrar}
             aria-labelledby="modal-titulo"
             aria-describedby="modal-cuerpo"
             >
-                <div>
-                    <h1 id="modal-titulo">MODAL DE INGRESO</h1>
-                    <p id="modal-cuerpo">Este es un modal de prueba</p>
-                    <button onClick={manejarCerrar}>CERRAR</button>
-                </div>
+                <Box sx={estiloModal}>
+                    <h1 id="modal-titulo">MODAL DE PRUEBA</h1>
+                    <p id="modal-cuerpo">Este es el texto del Modal de prueba</p>
+                    <button onClick={manejarCerrar} className="btn btn-danger">Cerrar</button>
+                </Box>
             </Modal>
             <button className="btn bg-white border border-primary text-primary">MODIFICAR</button>
             <button className="btn bg-white border border-primary text-primary">ELIMINAR</button>
